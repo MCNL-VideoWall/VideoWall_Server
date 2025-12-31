60b2a438ba471fccfdd77e4ed0f8d0b14ea0fccf
DoHyeonhak <ehgusgkr6@naver.com>
2025-12-31 12:07:10 +0900

    [Feat] SessionManager class 생성

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from fastapi import FastAPI, WebSocket
-from typing import Dict
+from typing import Dict, List
 
 app = FastAPI()
 
 # ----------------------------------------------------
 # 1. Session class
 class Session:
+
     def __init__(self, sessionId: int, sessionName: str):
         self.sessionId = sessionId
         self.name = sessionName
         self.slot: int = 1024
-        self.Clients: Dict[int, WebSocket] = {}
+        self.clients: Dict[int, WebSocket] = {}
+
+
+# 2. Session Manager class
+class SessionManager: 
+    
+    def __init__(self):
+        self.sessionList: Dict[int, Session] = {}   # HostId, Session
+
+    def createSession(self, hostId: int, name: str) -> Session:
+        newSession = Session(sessionId=hostId, sessionName=name)
+        self.sessionList[hostId] = newSession
+        return newSession
+
+    def getSessionList(self):
+        result = []
+        for session in self.sessionList.values():
+            result.append({
+                "sessionId": session.sessionId,
+                "sessionName": session.name,
+                "currentClients": len(session.Clients), 
+                "maxSlots": session.slot
+            })
+        return result
 
 
 @app.websocket("/ws")
