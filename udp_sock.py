b9177cea9fab13a9a032e7ca3c96126c4d22b023
Jangmyun <cndlsrb2739@gmail.com>
2026-01-01 01:24:53 +0900

    [Feat]: udp 서버로직을 멀티프로세싱을 위해 함수로 분리

--- udp_sock.py
+++ udp_sock.py
@@ -1,17 +1,17 @@
 import socket
 
-BRDCAST_ADDR = "0.0.0.0"
-PORT = 65535
 
-udp_server_sock = socket.socket(
-    family=socket.AF_INET,
-    type=socket.SOCK_DGRAM
-)
+def run_udp_server(host: str = "0.0.0.0", port: int = 65535):
+    udp_server_sock = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_DGRAM
+    )
 
-udp_server_sock.bind((BRDCAST_ADDR, PORT))
+    udp_server_sock.bind((BRDCAST_ADDR, PORT))
 
-while True:
-    data, addr = udp_server_sock.recvfrom(100)
+    while True:
+        data, addr = udp_server_sock.recvfrom(100)
 
-    if data.decode().strip() == "VIDEO_WALL_CONNECT_REQUEST":
-        udp_server_sock.sendto("VIDEO_WALL_CONNECT_RESPONSE".encode(), addr)
+        if data.decode().strip() == "VIDEO_WALL_CONNECT_REQUEST":
+            udp_server_sock.sendto(
+                "VIDEO_WALL_CONNECT_RESPONSE".encode(), addr)
