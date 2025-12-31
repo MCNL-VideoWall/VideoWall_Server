857b304bc118d153c1884ab16122f23bfcf7f8c4
Jangmyun <cndlsrb2739@gmail.com>
2026-01-01 01:53:24 +0900

    [Feat]: FastAPI 서버 시작 시 UDP Server 프로세스 생성

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from fastapi import FastAPI, WebSocket
 from typing import Dict
+from contextlib import asynccontextmanager
+import multiprocessing
+from udp_sock import run_udp_server
 
 app = FastAPI()
 
 # ----------------------------------------------------
 # 1. Session class
+
+
 class Session:
     def __init__(self, sessionId: int, sessionName: str):
         self.sessionId = sessionId
         self.name = sessionName
         self.slot: int = 1024
         self.Clients: Dict[int, WebSocket] = {}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Server Startup Routine
+    print("UDP Server Starting Up...")
+    udp_process = multiprocessing.Process(
+        target=run_udp_server,
+        args=("0.0.0.0", 65535),
+        daemon=True
+    )
+    udp_process.start()
+    print("UDP Server On")
+
+    # Server Cleanup Routine
+    yield
 
 
 @app.websocket("/ws")
